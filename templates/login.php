aed7b3979007687c577d8ad59aed71d60dda6c5c
Nadiyah myrilla <143943300+nadiyahmyrilla@users.noreply.github.com>
2025-06-09 18:37:55 +0700

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Login Admin</title>
-    <link rel="stylesheet" href="../statics/login.css">
+  <meta charset="UTF-8">
+  <title>Login Admin</title>
+  <link rel="stylesheet" href="../statics/login.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
-<body class="login-body">
-    <div class="container">
-        <h2 class="title">Log In</h2>
+<body>
+  <div class="container">
+    <div class="left">
+      <div class="login-box">
+        <h2>Log In</h2>
         <form method="POST" action="../proses/login.php">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" required>
+          <label for="username">Username</label>
+          <input type="text" name="username" required>
 
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
+          <label for="password">Password</label>
+          <input type="password" name="password" required>
 
-            <a class="link" href="register.php">Tambah User Baru</a>
+          <a class="link" href="register.php">Tambah User Baru</a>
 
-            <button type="submit" class="button">LOGIN</button>
+          <button type="submit">LOGIN</button>
         </form>
+      </div>
     </div>
+    <div class="right">
+      <!-- Gambar diatur sebagai background di CSS -->
+    </div>
+  </div>
 </body>
 </html>
