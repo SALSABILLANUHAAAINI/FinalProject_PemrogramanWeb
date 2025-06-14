a0b0a34150523ce036f0a6a02dc3549cb9116580
Nadiyah myrilla <143943300+nadiyahmyrilla@users.noreply.github.com>
2025-06-14 12:21:35 +0700

    Update tambah_barang.php

--- proses/tambah_barang.php
+++ proses/tambah_barang.php
@@ -2,11 +2,14 @@
 include '../config/koneksi.php';
 
 $nama = $_POST['nama'];
-$stok = $_POST['stok'];
+$stok_awal = (int)$_POST['stok']; // ini adalah stok awal
 $kondisi = $_POST['kondisi'];
+$stok = $stok_awal; // stok sisa awalnya sama dengan stok_awal
 
-mysqli_query($conn, "INSERT INTO barang (nama_barang, stok, stok_awal, kondisi) VALUES ('$nama', $stok, $stok, '$kondisi')");
+mysqli_query($conn, "INSERT INTO barang (nama_barang, stok_awal, stok, kondisi) 
+                     VALUES ('$nama', $stok_awal, $stok, '$kondisi')");
 
 header("Location: ../templates/dataBarang.php?success=1");
 ?>
+
 
